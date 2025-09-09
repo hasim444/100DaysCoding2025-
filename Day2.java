20c736a2d38606c8624dfeae95d2d0a2939defdc
muh. hasyim <h13880134@gmail.com>
2025-09-09 16:15:23 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //integer (int)
+        int umur = 18;
+        double tinggi = 170.5;
+        boolean isStudent = true;
+        String nama = "Muh.hasyim";
+        
+        //double floating (double)
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi + " cm");
+        System.out.println("Mahasiswa: " + isStudent);
+    }
+}
